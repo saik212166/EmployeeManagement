6b42d3bbb9a4f9424e49a8178c6234f356441685
saik212166 <mssaikalyan2004@gmail.com>
2025-06-17 00:37:07 +0530

    Updated

--- src/main/java/com/example/EmployeeManagement/util/PasswordHashGenerator.java
+++ src/main/java/com/example/EmployeeManagement/util/PasswordHashGenerator.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordHashGenerator {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "saik212166";  // change this
+        String rawPassword = "saik212166";  
         String hashedPassword = encoder.encode(rawPassword);
         System.out.println("Hashed password: " + hashedPassword);
     }
